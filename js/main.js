8e1c0c0623c2e59514b3b298b33fb6fd99981497
Akhatmirzo <blackprincepbg@gmail.com>
2023-11-12 15:58:37 +0500

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 //  let, var  =  String Oject Arr Number Float double
 // const = String Oject Arr Number Float double
 
+const menuBtn = document.querySelector('.menuBtn');
+const navbarList = document.querySelector('.navbar__list');
 
-
-
+menuBtn.addEventListener('click', function() {
+    navbarList.classList.toggle('navbar__active');
+});
